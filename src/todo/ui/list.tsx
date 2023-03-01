b1ec73fc73c49e645d448cf9273e56aaa1a5e573
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-01 18:56:47 -0500

    Create list.tsx

---
+++ src/todo/ui/list.tsx
@@ -0,0 +1,25 @@
+import { Box, Paper, Stack } from '@mui/material'
+import { AddToDoForm } from '../forms/add'
+import { useToDoLocalStore } from '../stores/local'
+import { ToDoListHeader } from './list-header'
+import { ToDoListItem } from './list-item'
+
+export const ToDoList: React.FC = () => {
+  const { todos } = useToDoLocalStore()
+
+  return (
+    <Stack
+      component={Paper}
+      spacing={2}
+      flexGrow={1}
+      p={3}
+      borderRadius={2}
+    >
+      <ToDoListHeader />
+      <Stack spacing={1} flexGrow={1}>
+        {todos.map((todo) => (<ToDoListItem key={todo.uid} {...todo} />))}
+      </Stack>
+      <AddToDoForm />
+    </Stack>
+  )
+}
