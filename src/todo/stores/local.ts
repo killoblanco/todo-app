d37ea9feb9313d689a93cb5d99c0a0a67c87004a
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-02 01:43:01 -0500

    Update local.ts

--- src/todo/stores/local.ts
+++ src/todo/stores/local.ts
@@ -1,11 +1,24 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
-import { type TodoLocalStorage } from '../types'
+import { TODO_STATUS } from '../consts'
+import { type Todo, type TodoLocalStorage } from '../types'
 
 export const useToDoLocalStore = create(persist<TodoLocalStorage>(
-  (set) => ({
+  (set, get) => ({
     todos: [],
-    add: (todo) => { set((state) => ({ todos: [...state.todos, todo] })) }
+    actives: get().todos.filter((todo) => todo.status === TODO_STATUS.ACTIVE),
+    completed: get().todos.filter((todo) => todo.status === TODO_STATUS.COMPLETED),
+    add: (todo) => { set((state) => ({ todos: [...state.todos, todo] })) },
+    toggleComplete: ({ uid }: Pick<Todo, 'uid'>) => {
+      set((state) => ({
+        todos: state.todos.map((todo) => todo.uid === uid
+          ? {
+              ...todo,
+              status: todo.status === TODO_STATUS.ACTIVE ? TODO_STATUS.COMPLETED : TODO_STATUS.ACTIVE
+            }
+          : todo)
+      }))
+    }
   }),
   {
     name: 'todo-local-store',
