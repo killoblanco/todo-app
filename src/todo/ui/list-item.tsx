1008b32b6082d64b144e00e0ae38246101ac935b
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-01 19:02:29 -0500

    Update list-item.tsx

--- src/todo/ui/list-item.tsx
+++ src/todo/ui/list-item.tsx
@@ -2,7 +2,7 @@ import { Paper, Stack, Typography } from '@mui/material'
 import { type Todo } from '../types'
 
 export const ToDoListItem: React.FC<Todo> = (todo: Todo) => (
-  <Stack direction="row" spacing={2} component={Paper} p={2} variant="outlined">
+  <Stack direction="row" spacing={2} component={Paper} px={2} py={1} variant="outlined">
     <Typography>{todo.text}</Typography>
   </Stack>
 )
