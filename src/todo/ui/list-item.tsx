e2f0504c22f6290de2e92349b21ed45a42a03c16
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-02 01:41:08 -0500

    Update list-item.tsx

--- src/todo/ui/list-item.tsx
+++ src/todo/ui/list-item.tsx
@@ -1,8 +1,20 @@
-import { Paper, Stack, Typography } from '@mui/material'
+import { Checkbox, Paper, Stack, Typography } from '@mui/material'
+import { TODO_STATUS } from '../consts'
+import { useToDoLocalStore } from '../stores/local'
 import { type Todo } from '../types'
 
-export const ToDoListItem: React.FC<Todo> = (todo: Todo) => (
-  <Stack direction="row" spacing={2} component={Paper} px={2} py={1} variant="outlined">
-    <Typography>{todo.text}</Typography>
-  </Stack>
-)
+export const ToDoListItem: React.FC<Todo> = (todo: Todo) => {
+  const { toggleComplete } = useToDoLocalStore()
+
+  return (
+    <Stack direction="row" spacing={2} component={Paper} px={2} py={1} variant="outlined" alignItems="center">
+      <Checkbox
+        checked={todo.status === TODO_STATUS.COMPLETED}
+        onChange={() => {
+          toggleComplete({ uid: todo.uid })
+        }}
+      />
+      <Typography>{todo.text}</Typography>
+    </Stack>
+  )
+}
