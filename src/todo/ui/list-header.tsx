faf3dfb4869b4f86edd14fba5dff366bcd8f81a5
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-02 01:59:45 -0500

    Update list-header.tsx

--- src/todo/ui/list-header.tsx
+++ src/todo/ui/list-header.tsx
@@ -1,10 +1,17 @@
-import { Divider, Stack, Typography } from '@mui/material'
+import { Box, Divider, Stack, Typography } from '@mui/material'
+import { useToDoLocalStore } from '../stores/local'
 
-export const ToDoListHeader: React.FC = () => (
-  <>
-    <Stack direction="row" spacing={2}>
-      <Typography variant="h5">To Do List</Typography>
-    </Stack>
-    <Divider flexItem />
-  </>
-)
+export const ToDoListHeader: React.FC = () => {
+  const { getActives } = useToDoLocalStore()
+
+  return (
+    <>
+      <Stack direction="row" spacing={2}>
+        <Typography variant="h5">To Do List</Typography>
+        <Box flexGrow={1} />
+        <Typography variant="h5">{`${getActives().length} Tasks`}</Typography>
+      </Stack>
+      <Divider flexItem />
+    </>
+  )
+}
