4a161da4405dc08481ea94def60b2241832c64cc
Kamilo Vasquez <killo.blanco@gmail.com>
2023-03-02 01:59:51 -0500

    Update complete-list.tsx

--- src/todo/ui/complete-list.tsx
+++ src/todo/ui/complete-list.tsx
@@ -1,24 +1,29 @@
-import { Accordion, AccordionDetails, AccordionSummary, Stack, Typography } from '@mui/material'
-import { TODO_STATUS } from '../consts'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Stack, Typography } from '@mui/material'
 import { useToDoLocalStore } from '../stores/local'
 import { ToDoListItem } from './list-item'
 import { Icon } from '@iconify/react'
 
 export const ToDoCompleteList: React.FC = () => {
-  const { completed } = useToDoLocalStore()
+  const { getCompleted } = useToDoLocalStore()
+  const todos = getCompleted()
+  const size = todos.length
 
-  if (completed.length <= 0) return null
+  if (size <= 0) return null
 
   return (
     <Accordion>
       <AccordionSummary
         expandIcon={<Icon icon="tabler:chevron-down" width={24} height={24} />}
       >
-        <Typography>Completed</Typography>
+        <Stack direction="row" spacing={2} flexGrow={1}>
+          <Typography>Completed</Typography>
+          <Box flexGrow={1} />
+          <Typography>{`${size} Tasks`}</Typography>
+        </Stack>
       </AccordionSummary>
       <AccordionDetails>
         <Stack spacing={1}>
-          {completed.map((todo) => (<ToDoListItem key={todo.uid} {...todo} />))}
+          {todos.map((todo) => (<ToDoListItem key={todo.uid} {...todo} />))}
         </Stack>
       </AccordionDetails>
     </Accordion>
